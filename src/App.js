48695aeff8ad82d8198ade5754eb9d7542f23fa4
toyman640 <falakoadebayor@gmail.com>
2024-01-24 14:04:37 +0100

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
+import MainPage from './components/Main';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<SignUp />} />
           <Route exact path="/log-in" element={<LogIn />} />
+          <Route exact path="/home-page" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainPage = () => (
+  <div className="MainContainer">
+    <div>
+      1
+    </div>
+    <div>
+      2
+    </div>
+  </div>
+);
+
+export default MainPage;
