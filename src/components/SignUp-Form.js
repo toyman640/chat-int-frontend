e74ad6b6e3321e7bd0211ce8b8dab22f260eadee
toyman640 <falakoadebayor@gmail.com>
2024-01-24 11:28:59 +0100

    Set up Sign up form Component

---
+++ src/components/SignUp-Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SignUpForm = () => {
+  <form>
+    <div>
+      <label>Username</label>
+      <input type="text" name="username" />
+    </div>
+    <div>
+      <label>email</label>
+      <input type="email" name="email" />
+    </div>
+    <div>
+      <input type="Submit" value="Sign Up" />
+    </div>
+  </form>;
+};
+
+export default SignUpForm;
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from '../home-logo.svg';
+import SignUpForm from './SignUp-Form';
 
 const SignUp = () => (
   <>
     <div className="SignUpCover">
-      This is Sign up Component
+      <div className="SignUpContent">
+        <img src={logo} alt="logo" className="logo-img" />
+        <div>
+          <h2>Sign Up</h2>
+          <p>Create an account</p>
+          <SignUpForm />
+        </div>
+      </div>
     </div>
   </>
 );
